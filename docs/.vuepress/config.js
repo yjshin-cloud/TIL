b262429acd4544888f931291e01f73724bf939b3
yjshin.cloud <yjshin.cloud@gmail.com>
2025-08-28 00:30:36 +0900

    TEST5

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+// docs/.vuepress/config.js
 const { defineUserConfig } = require('vuepress')
 const { viteBundler } = require('@vuepress/bundler-vite')
 const { defaultTheme } = require('@vuepress/theme-default')
@@ -9,6 +10,7 @@ module.exports = defineUserConfig({
     title: 'YJ Shin TIL Blog',
     description: 'Today I Learned',
     bundler: viteBundler(),
+
     theme: defaultTheme({
         navbar: [
             { text: '홈', link: '/' },
@@ -17,16 +19,22 @@ module.exports = defineUserConfig({
             { text: 'Edu', link: '/Edu/' },
             { text: 'OS', link: '/OS/' },
             { text: 'Programming', link: '/Programming_Languages/' },
-            { text: 'GitHub', link: 'https://github.com/yjshin-cloud/TIL' },
+            { text: 'GitHub', link: 'https://github.com/yjshin-cloud/TIL' }
         ],
+
+        // ⬇️ 폴더 구조 자동 사이드바
         sidebar: 'structure',
+
+        // 편집 링크 정확도 향상(문서 루트가 docs 이므로)
         repo: 'yjshin-cloud/TIL',
         docsRepo: 'yjshin-cloud/TIL',
         docsBranch: 'main',
         docsDir: 'docs',
+
         editLink: true,
         lastUpdated: true,
         contributors: true,
     }),
-    plugins: [searchPlugin()]
+
+    plugins: [searchPlugin({ maxSuggestions: 10 })],
 })
